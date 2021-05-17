acacbd344fac6362b02f6b8ab6d2f87539727c20
Chris Wade <chris@wadespace.net>
2021-05-17 17:20:50 +1000

    initial

---
+++ src/components/MainHeader.js
@@ -0,0 +1,17 @@
+const MainHeader = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/welcome"></a>
+          </li>
+          <li>
+            <a href="/products"></a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default MainHeader;
